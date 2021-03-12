a16d2dceed3df77a3c38e12bde595e8e7888c67d
cottojon <cottojonathan@ymail.com>
2021-03-12 15:30:12 -0500

    spring initalizer commit

---
+++ src/main/java/com/landon/hotel/SpringReactiveHotelLandonApplication.java
@@ -0,0 +1,13 @@
+package com.landon.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveHotelLandonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactiveHotelLandonApplication.class, args);
+	}
+
+}
